d36b8d7288c1a1abd772b48f96febe7db82e776e
Liews Wuttipat <sweilz.w@gmail.com>
2018-07-11 21:07:01 +0700

    update

--- leelawadee_mbed/scripts/test_drive.py
+++ leelawadee_mbed/scripts/test_drive.py
@@ -9,9 +9,8 @@ goal_linear_velocity = 0.0
 goal_angular_velocity = 0.0
 
 def setSpeedRaw(RawSpeedL,RawSpeedR):
-    command = "#1P%s#2P%sT500" % (str(RawSpeedL), str(RawSpeedR))
+    command = "#1P{}#2P{}T500".format(RawSpeedL, RawSpeedR)
     rospy.loginfo(command)
-    # print(command)
 
 def cmdvelCB(msg):
     global goal_linear_velocity,goal_angular_velocity
@@ -20,22 +19,19 @@ def cmdvelCB(msg):
 
 def timerCB(event):
     global goal_linear_velocity,goal_linear_velocity
-    speedL = goal_linear_velocity - (goal_angular_velocity * 0.2)
-    speedR = goal_linear_velocity + (goal_angular_velocity * 0.2)
-    speedL = 1500 + speedL * 1000/(2.19*0.15)
-    speedR = 1500 + speedR * 1000/(2.19*0.15)
+    speedL = goal_linear_velocity - (goal_angular_velocity * 0.25)
+    speedR = goal_linear_velocity + (goal_angular_velocity * 0.25)
+    speedL = 1500 + speedL * 1000/(2.19*0.1)
+    speedR = 1500 - speedR * 1000/(2.19*0.1)
     setSpeedRaw(int(speedL) ,int(speedR))
 
 def main():
     rospy.init_node('Test_Drive')
+    
     rospy.Subscriber('/cmd_vel',Twist,cmdvelCB)
-    timer = rospy.Timer(rospy.Duration(0.5),timerCB)
+    timer = rospy.Timer(rospy.Duration(0.25),timerCB)
     rospy.spin()
     timer.shutdown()
-
-    # setSpeedRaw(100,500)
-    # setSpeedRaw(900,2500)
-    # pass
 
 if __name__ == '__main__':
     main()
