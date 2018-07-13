597f8b5829fc6efa5a8d317f85f99a742f47c989
sweilz.w@gmail.com <sweilz-w@localhost.localdomain>
2018-07-13 15:53:55 +0000

    from rpi

--- leelawadee_mbed/scripts/test_drive.py
+++ leelawadee_mbed/scripts/test_drive.py
@@ -3,14 +3,15 @@ import rospy
 import serial
 from geometry_msgs.msg import Twist
 
-# ser = serial.Serial('/dev/ttyUSB0',baudrate=115200)
+ser = serial.Serial('/dev/ttyUSB0',baudrate=115200)
 
 goal_linear_velocity = 0.0
 goal_angular_velocity = 0.0
 
 def setSpeedRaw(RawSpeedL,RawSpeedR):
-    command = "#1P{}#2P{}T500".format(RawSpeedL, RawSpeedR)
+    command = "#1P{}#2P{}T100\r\n".format(RawSpeedL, RawSpeedR)
     rospy.loginfo(command)
+    ser.write(command)
 
 def cmdvelCB(msg):
     global goal_linear_velocity,goal_angular_velocity
@@ -29,7 +30,11 @@ def main():
     rospy.init_node('Test_Drive')
     
     rospy.Subscriber('/cmd_vel',Twist,cmdvelCB)
-    timer = rospy.Timer(rospy.Duration(0.25),timerCB)
+    ser.write("#255PCLE\r\n")
+    rospy.sleep(1)
+    ser.write("#255PMOD7\r\n")
+    rospy.sleep(1)
+    timer = rospy.Timer(rospy.Duration(0.1),timerCB)
     rospy.spin()
     timer.shutdown()
 
