5c63cd15e057fc8260085a9c060cbf5d785ae2a8
Liews Wuttipat <sweilz.w@gmail.com>
2018-07-15 21:31:27 +0700

    update movebase

--- leelawadee_mbed/scripts/base_control.py
+++ leelawadee_mbed/scripts/base_control.py
@@ -23,11 +23,11 @@ class BaseControl(object):
         self.odom_freq = float(rospy.get_param("~odom_freq", "50"))
         self.cmd_freq = float(rospy.get_param("~cmd_freq", "10"))
 
-        # try:
-        #     self.serial = serial.Serial(self.port,self.baudrate,timeout=10)
-        # except serial.serialutil.SerialException:
-        #     rospy.logerr("Cannot connect to port: " + self.port + ".")
-        #     sys.exit(0)
+        try:
+            self.serial = serial.Serial(self.port,self.baudrate,timeout=10)
+        except serial.serialutil.SerialException:
+            rospy.logerr("Cannot connect to port: " + self.port + ".")
+            sys.exit(0)
         rospy.loginfo("Communication success!")
 
         self.sub = rospy.Subscriber(
@@ -68,20 +68,23 @@ class BaseControl(object):
                                 1000.0/self.MAX_W, 500, 2500)
         command = "#1P{}#2P{}T1\r\n".format(int(speedL), int(speedR))
         # rospy.logwarn(command)
-        # self.serial.write(command)
+        self.serial.write(command)
 
     def timerOdomCB(self, event):
-        speedL = self.sendWL * self.wheelRad
-        speedR = self.sendWR * self.wheelRad
-        speedTh = (speedR - speedL)/self.wheelSep
-        speedX = (speedR + speedL)/2.0
+        # speedL = self.sendWL * self.wheelRad
+        # speedR = self.sendWR * self.wheelRad
+        # speedTh = (speedR - speedL)/self.wheelSep
+        # speedX = (speedR + speedL)/2.0
 
         self.current_time = rospy.Time.now()
         dt = (self.current_time - self.previous_time).to_sec()
         self.previous_time = self.current_time
-        self.pose_x += speedX * math.cos(self.pose_th) * dt
-        self.pose_y += speedX * math.sin(self.pose_th) * dt
-        self.pose_th += speedTh * dt
+        # self.pose_x += (speedX * math.cos(self.pose_th) * dt)
+        # self.pose_y += (speedX * math.sin(self.pose_th) * dt)
+        # self.pose_th += (speedTh * dt)
+        self.pose_x += (self.trans_x * math.cos(self.pose_th) * dt)
+        self.pose_y += (self.trans_x * math.sin(self.pose_th) * dt)
+        self.pose_th += (self.rotat_z * dt)
         pose_quat = tf.transformations.quaternion_from_euler(
             0, 0, self.pose_th)
 
@@ -95,8 +98,10 @@ class BaseControl(object):
         msg.pose.pose.orientation.y = pose_quat[1]
         msg.pose.pose.orientation.z = pose_quat[2]
         msg.pose.pose.orientation.w = pose_quat[3]
-        msg.twist.twist.linear.x = speedX
-        msg.twist.twist.angular.z = speedTh
+        # msg.twist.twist.linear.x = speedX
+        # msg.twist.twist.angular.z = speedTh
+        msg.twist.twist.linear.x = self.trans_x
+        msg.twist.twist.angular.z = self.rotat_z
         for i in range(36):
             msg.twist.covariance[i] = 0
         msg.twist.covariance[0] = 1.0  # speedX Cov
@@ -105,7 +110,7 @@ class BaseControl(object):
         self.pub.publish(msg)
 
         self.tf_broadcaster.sendTransform(
-            (self.pose_x, self.pose_y, self.pose_th), pose_quat, self.current_time, self.baseId, self.odomId)
+            (self.pose_x, self.pose_y, 0.0), pose_quat, self.current_time, self.baseId, self.odomId)
 
 
 if __name__ == '__main__':
